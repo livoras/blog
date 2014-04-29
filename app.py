793e480678171b3f3cf4a896dd2423e3b639d8e7
livoras <livoras@163.com>
2014-04-29 21:32:06 +0800

    add elegant 404 fallback to static file check

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import config
+import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from common import db
 
 app = Flask(__name__)
@@ -9,6 +10,15 @@ app.config.from_object(config)
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.errorhandler(404)
+def check_static(error):
+  path = 'static' + request.path
+  if os.path.exists(path):
+    return send_file(path)
+  else:  
+    return render_template('404.html'), 404
+
 
 db.init_db()
 
