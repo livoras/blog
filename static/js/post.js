5871229a53b185747ead58f97d5bb3a0063ad007
livoras <livoras@163.com>
2014-04-30 21:40:16 +0800

    show comments

--- static/js/post.js
+++ static/js/post.js
@@ -8,7 +8,19 @@
     var dataStr = $newCommentForm.serialize()
     var data = blog.utils.getJSONFromQueryStr(dataStr)
 
-    var promise = blog.utils.ajax({})
+    var promise = blog.utils.ajax({
+      url: '/new_comment',
+      type: 'post',
+      data: data
+    })
+
+    promise.success(function(data) {
+      console.log(data)
+    })
+
+    promise.error(function(error) {
+      alert('发送失败：' + error.responseJSON.error[0])
+    })
   })
 
 })()
