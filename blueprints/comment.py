deb36c0726461eefcc8b071971f0d0bca4d5a80f
livoras <livoras@163.com>
2014-05-07 16:06:04 +0800

    sending mail using mutil-threads

--- blueprints/comment.py
+++ blueprints/comment.py
@@ -1,11 +1,14 @@
 # coding=utf-8
 import config
+import thread
 
 from flask import Blueprint, jsonify, request, \
-                  render_template, session, abort
+                  render_template, session, abort, \
+                  url_for
 
 from business import comment
 from common import utils
+from common import mail
 from models.post import Post
 from models.comment import Comment
 
@@ -16,10 +19,18 @@ def create_new_comment():
   data = request.json
   new_comment = comment.create_new_comment(data)
   if isinstance(new_comment, Comment):
+    if not config.TESTING: 
+      mail_data = new_comment.get_dict()
+      thread.start_new_thread(send_mail, (mail_data, new_comment.post.id))
     return utils.success(new_comment.get_dict())
   else:  
     error = new_comment
     return utils.fail(error, 400)
+
+
+def send_mail(mail_data, post_id):
+  mail_data['link'] = 'http://livoras.com/post/%s' % post_id
+  mail.send(config.MAIL_CONTENT.format(**mail_data))
 
 
 @comment_bp.route('/delete_comment', methods=["DELETE"])
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 debug = False
 
 SECRET_KEY = '9687d209a9ff0713da8edae47dce398fuckyoublog'
@@ -18,3 +19,18 @@ ADMIN_EMAIL = 'livoras@163.com'
 TITLE = 'Livoras\' Blog'
 
 POSTS_PER_PAGE = 7
+TESTING = True
+
+# email settings
+SENDER = 'noreplylivoras@163.com'
+RECIEVER = 'me@livoras.com'
+SMTP_SERVER = 'smtp.163.com'
+SENDER_PASSWORD = 'crackmeplease.'
+MAIL_CONTENT = u'''
+<h1>Livoras</h1>
+<h2>有个傻逼给你评论了你一下！</h2>
+<div><a href="{link}">博客页面</a></div>
+<div>TA的名字：{username}</div>
+<div>TA的Email：{user_email}</div>
+<div>TA说：{content}</div>
+'''
