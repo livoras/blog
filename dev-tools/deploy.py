ba2faf487cf392ed0c36c6cc26126d0c3123cbfa
livoras <livoras@163.com>
2014-05-07 16:21:05 +0800

    remove livoreload before deploy

--- dev-tools/deploy.py
+++ dev-tools/deploy.py
@@ -5,5 +5,16 @@ def change_testing_mode():
   f.write('\nTESTING = False\n')
   f.close()
 
+def delete_live_reload():
+  f = file('templates/layout.html', 'r+')
+  text = f.read()
+  text = text.replace('<script src="//localhost:35729/livereload.js"></script>', '')
+  f.close()
+
+  f = file('templates/layout.html', 'w')
+  f.write(text)
+  f.close()
+
 if __name__ == '__main__':
   change_testing_mode()
+  delete_live_reload()
