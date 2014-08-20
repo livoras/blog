52cfc0a6ce997bc130d02873005a748809fcb079
livoras <livoras@163.com>
2014-08-20 15:49:32 +0800

    fuck

--- ws7/bin/js/main.js
+++ ws7/bin/js/main.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var $, $buttons, $links, $scroller, PADDING, addLinks, canvasHeight, canvasWidth, init, radius, resizeButtons, resizeScroller, rotationSnap, rotationSnapPI, scroll, sh, sw, util;
+var $, $buttons, $links, $scroller, PADDING, canvasHeight, canvasWidth, init, radius, resizeButtons, resizeScroller, rotationSnap, rotationSnapPI, scroll, sh, sw, util;
 
 util = require("./util.coffee");
 
@@ -30,7 +30,6 @@ $links = $("span.link");
 init = function() {
   resizeButtons();
   resizeScroller();
-  addLinks();
   return scroll();
 };
 
@@ -51,16 +50,6 @@ resizeScroller = function() {
   $scroller.style.height = c + "px";
   $scroller.style.webkitBorderRadius = c + "px";
   return $scroller.style.left = -PADDING + "px";
-};
-
-addLinks = function() {
-  return Array.prototype.forEach.call($links, function($link, i) {
-    return $link.addEventListener("click", function() {
-      var link;
-      link = $link.getAttribute("href");
-      return window.location.href = link;
-    });
-  });
 };
 
 scroll = function() {
