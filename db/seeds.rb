deeef9cd717413fafc2f44399b0d15538d83e3fd
livoras <livoras@163.com>
2015-04-28 11:31:57 +0800

    make good seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,24 @@
 
 User.create({email: "admin@admin.com", name: "admin", plain_password: "123456", plain_password_confirmation: "123456"})
 
-# posts = 100.times.map do |i|
-#   {title: "title - #{i}", content: "Fuck you every day for everyday.#{i}", user_id: 1}
-# end
+posts = [{user_id: 1, title: 'Hello, Simple Blog!', content: %{
+## 开始撰写你的博文吧！
 
-# Post.create(posts)
+Hello, Simple Blog!
 
-# Tag.create([
-#   {name: "tag1", post_id:1},
-#   {name: "tag2", post_id:1}
-# ])
+![block](/assets/demo.jpeg)
+
+你可以对博客进行自定义的配置：
+
+1. 修改`config/config.yml`修改博客的名称（`blog_name`）和描述（`blog_description`）。
+2. 博客管理员默认登陆邮箱和密码为`admin@admin.com`和`123456`，请及时[登录](/login)修改登录邮箱和密码。
+3. 修改`config/environments/development.rb`, `config/environments/production.rb`修改博客评论邮件提示邮箱配置。
+
+登录以后即可以进行博文、评论的管理，点击左上角的"New Post"进行新博文编写，采用Markdown语法生成博文内容。}}]
+
+Post.create(posts)
+
+Tag.create([{name: "启程", post_id:1}])
 
 # Comment.create([
 #   {email: "fuck@you.com", name: "Livoras", content: "I love this post", post_id: 1},
--- makefile
+++ makefile
@@ -2,6 +2,10 @@ dev :
 	RAILS_ENV=development rails server
 compile:
 	RAILS_ENV=production bin/rake assets:precompile
+reset:
+	rake db:drop
+	rake db:schema:load
+	rake db:seed
 prod :
 	RAILS_DB_NAME=blog RAILS_DB_USER=blog RAILS_DB_PASSWORD=111111 RAILS_ENV=production SECRET_KEY_BASE=averysecretkey RAILS_SERVE_STATIC_FILES=true RAILS_ENV=production rails server
 launch:
